9459ae70e2e6e7b17e0d123d0e0b41f28b9f7ebb
progart116 <prog.art.116@yandex.ru>
2025-05-25 22:08:22 +0300

    function: mark task as completed

--- ToDoListApp/views.py
+++ ToDoListApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import django.urls
 import django.views.decorators.http
 import django.contrib.auth.decorators
 import django.http
@@ -12,7 +13,7 @@ def view_task_list(request):
     try:
         context = {
             "user": UserLogApp.models.User.objects.get(django_auth_user__username=str(request.user)),
-            "tasks": ToDoListApp.models.Task.objects.all(),
+            "tasks": ToDoListApp.models.Task.objects.filter(user__django_auth_user__username=str(request.user)),
         }
         UserLogApp.models.Logs.objects.create(log_data=f"Открыта страница отображения списка задач", app_name="ToDoListApp", method_name="views.view_task_list", program_user=str(request.user))
         return render(request, "ToDoListApp/list.html", context)
@@ -36,6 +37,16 @@ def add_task_post(request):
 
 
 @django.contrib.auth.decorators.login_required
-@django.views.decorators.http.require_http_methods(["POST"])
 def complete_task_post(request, id_task):
+    try:
+        task = ToDoListApp.models.Task.objects.get(id=id_task)
+        if task.user.django_auth_user.username != str(request.user): return django.http.HttpResponseForbidden()
+        else:
+            task.completed = True
+            task.save()
+            UserLogApp.models.Logs.objects.create(log_data=f"Задача {task.id} завершена", app_name="ToDoListApp", method_name="views.complete_task_post", program_user=str(request.user))
+            return django.http.HttpResponseRedirect(django.urls.reverse("ToDoListApp:view_task_list"))        
+    except Exception as ex:
+        UserLogApp.models.Logs.objects.create(log_data=f"Ошибка: {str(ex)}", app_name="ToDoListApp", method_name="views.complete_task_post", program_user=str(request.user))
+        return django.http.HttpResponseServerError()
     return django.http.HttpResponse("Not implemented: complete post")
