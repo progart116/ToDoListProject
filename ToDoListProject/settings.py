188062afd2a0b91fc6ff9bc7aa6a2caf77b590fd
progart116 <prog.art.116@yandex.ru>
2025-05-25 14:29:42 +0300

    create apps and include to project

--- ToDoListProject/settings.py
+++ ToDoListProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ToDoListProject.private
+SECRET_KEY = ToDoListProject.private.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "ToDoListApp",
+    "UserLogApp",
 ]
 
 MIDDLEWARE = [
